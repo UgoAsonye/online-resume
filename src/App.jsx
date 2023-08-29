b5b8ae3483cdc7460493755aedc76b7c9c0d86cc
Ugo Asonye <ugonnaasonye@yahoo.com>
2023-08-28 20:39:10 -0500

    Add in cards for Resume

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Header } from "./Header";
 import React from "react";
 import { Footer } from "./Footer";
 import { Content } from "./Content";
+import { Resume } from "./Resume";
 
 function App() {
   return (
     <div>
       <Header />
       <Content />
+      <Resume />
       <Footer />
     </div>
   );
---
+++ src/Resume.jsx
@@ -0,0 +1,57 @@
+export function Resume() {
+  return (
+    <div id="Resume">
+      <h1 className="text-center text-uppercase">Joe Schmoe</h1>
+      <h1 className="text-center text-uppercase">Software Engineer Extraordinaire</h1>
+      <p className="text-center">
+        Metropolis, USA | 123-456-7890 | joeschmoe@test.com | github.com/joeschmoe | linekdin/com/in/joe-schmoe
+      </p>
+
+      <div className="row row-cols-1 row-cols-md-1 g-4">
+        <div className="col">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                This is a longer card with supporting text below as a natural lead-in to additional content. This
+                content is a little bit longer.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                This is a longer card with supporting text below as a natural lead-in to additional content. This
+                content is a little bit longer.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                This is a longer card with supporting text below as a natural lead-in to additional content.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                This is a longer card with supporting text below as a natural lead-in to additional content. This
+                content is a little bit longer.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
